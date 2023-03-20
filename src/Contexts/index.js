19bbc5d54281d352d5f55b5edc7ffb4c55b53744
lucas <lucasbezerra.rocinha@gmail.com>
2023-03-20 16:39:06 -0300

    Manipulando cadastro com Context API e Firebase

--- src/Contexts/index.js
+++ src/Contexts/index.js
@@ -1,13 +1,47 @@
 import { useState, createContext, useEffect } from 'react';
 
+import { db, auth } from '../Services/firebaseConnection';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+
 export const AuthContext = createContext({});
 
 export default function AuthProvider({ children }) {
 
+    const [loadingAuth, setLoadingAuth] = useState(false);
     const [user, setUser] = useState(null);
 
     function signIn(email, password) {
         alert('Logado com Sucesso ' + email + password)
+    }
+
+    async function signUp(email, password, name) {
+        setLoadingAuth(true)
+
+        await createUserWithEmailAndPassword(auth, email, password)
+        .then(async (value) => {
+            let uid = value.user.uid;
+            
+            await setDoc(doc(db, "users", uid), {
+                nome: name,
+                avatarUrl: false,
+            })
+            .then(() => {
+                let data = {
+                    uid: uid,
+                    nome: name,
+                    email: value.user.email,
+                    avatarUrl: null
+                }
+
+                setUser(data);
+                setLoadingAuth(false);
+            })
+        })
+        .catch((error) => {
+            console.log(error);
+            setLoadingAuth(false);
+        })
     }
 
     return(
@@ -16,6 +50,8 @@ export default function AuthProvider({ children }) {
             signed: !!user,
             user,
             signIn,
+            signUp,
+            loadingAuth
         }}
         >
             {children}
--- src/Pages/SignUp/index.js
+++ src/Pages/SignUp/index.js
@@ -1,12 +1,27 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/logo.png'
+
+import { AuthContext } from '../../Contexts';
 
 export default function SignUp() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
+
+    const { signUp, loadingAuth } = useContext(AuthContext);
+
+    function handleSignUp(event) {
+        event.preventDefault();
+
+        if(email !== '' && password !== '' && name !== '') {
+            signUp(email, password, name)
+            setEmail('');
+            setName('');
+            setPassword('');
+        }
+    }
 
     return(
         <div className='container-center'>
@@ -15,7 +30,7 @@ export default function SignUp() {
                     <img src={Logo} alt='Logo do sistema de chamados'/>
                 </div>
 
-                <form>
+                <form onSubmit={handleSignUp}>
                     <h1>Nova conta</h1>
                     <input 
                     type='text'
@@ -35,7 +50,7 @@ export default function SignUp() {
                     value={password}
                     onChange={(event) => setPassword(event.target.value)}
                     />
-                    <button type='submit'>Cadastrar</button> 
+                    <button type='submit'>{loadingAuth ? 'Carregando...' : 'Cadastrar'}</button> 
                 </form>
 
                 <Link to='/'>Já possui uma conta? Faça login</Link>
