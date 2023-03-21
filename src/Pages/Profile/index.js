ff43218151dbefdc22eee4afbbf1d0ae2d30ddd4
lucas <lucasbezerra.rocinha@gmail.com>
2023-03-21 17:15:15 -0300

    Criação do profile e estilização

--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -1,10 +1,18 @@
 import './style.css'
 import Header from '../../Components/Header';
 import Title from '../../Components/Title';
+import avatar from '../../assets/avatar.png'
+import { FiSettings, FiUpload } from 'react-icons/fi';
 
-import { FiSettings } from 'react-icons/fi';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../Contexts';
 
 export default function Profile() {
+
+    const { user } = useContext(AuthContext);
+
+    const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
+
     return (
         <div>
             <Header />
@@ -13,9 +21,35 @@ export default function Profile() {
                 <Title name="Minha conta">
                     <FiSettings size={25} />
                 </Title>
+            
+                <div className='container'>
+                    <form className='form-profile'>
+                        <label className='label-avatar'>
+                            <span>
+                                <FiUpload color="#FFF" size={25}/>
+                            </span>
+
+                            <input type="file" accept="image/*" /><br/>
+                            {avatarUrl === null ? (
+                                <img src={avatar} alt="Foto de perfil" width={250} height={250}/>
+                            ) : (
+                                <img src={avatarUrl} alt="Foto de perfil" width={250} height={250}/>
+                            )}
+                        </label>
+
+                        <label>Nome</label>
+                        <input type="text" placeholder='Seu nome'/>
+                        <label>Email</label>
+                        <input type="email" placeholder='email@email.com' disabled={true}/>
+                        <button type="submit">Salvar</button>
+                    </form>
+                </div>
+
+                <div className='container'>
+                    <button className='logout-btn'>Sair</button>
+                </div>
             </div>
 
-            <h1>Página perfil</h1>
         </div>
     )
 }
